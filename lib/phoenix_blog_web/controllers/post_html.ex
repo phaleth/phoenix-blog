a03e2d033d5144d94083afec75dd45a9b7818093
phaleth <29838606+phaleth@users.noreply.github.com>
2023-07-24 19:40:22 +0200

    Generate Post resource and all the layers using html generator

---
+++ lib/phoenix_blog_web/controllers/post_html.ex
@@ -0,0 +1,13 @@
+defmodule PhoenixBlogWeb.PostHTML do
+  use PhoenixBlogWeb, :html
+
+  embed_templates "post_html/*"
+
+  @doc """
+  Renders a post form.
+  """
+  attr :changeset, Ecto.Changeset, required: true
+  attr :action, :string, required: true
+
+  def post_form(assigns)
+end
